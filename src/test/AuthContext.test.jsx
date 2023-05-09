0be66af2b42cb8b829619330e88f1e767cf878c3
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-05-09 11:21:34 +0300

    Added 4 Unit Tests

--- src/test/AuthContext.test.jsx
+++ src/test/AuthContext.test.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { configure, shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { AuthContextProvider } from "../context/AuthContext";
-import AuthContext from "../context/AuthContext";
 
 configure({ adapter: new Adapter() });
 
